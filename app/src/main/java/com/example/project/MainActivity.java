1820157a90e7076b0720205bdc65a190f89bf209
b21antbr <b21antbr@student.his.se>
2022-05-05 19:32:58 +0200

    start on sharedpreferences

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     Button activityButton;
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
+    private EditText mName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         activityButton = findViewById(R.id.activityButton);
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,13 +1,24 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
-    TextView secondTextView;
+    private TextView mName;
 
-    protected void onCreate(){
-        secondTextView = findViewById(R.id.secondTextView);
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+        mName = (TextView) findViewById(R.id.secondTextView);
+
+        SharedPreferences mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = mPreferences.edit();
+        String name = mPreferences.getString(getString(R.string.name),"");
+        mName.setText(name);
     }
 }
